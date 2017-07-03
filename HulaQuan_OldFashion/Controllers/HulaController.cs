7ba5f9e94695a2a6d6d0efee239430dc5b9ced9e
Yupeng Li <allenhula@gmail.com>
2017-07-03 18:06:55 +0800

    Add project files.

---
+++ HulaQuan_OldFashion/Controllers/HulaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HulaQuan_OldFashion.Controllers
+{
+    public class HulaController : Controller
+    {
+        // GET: Hula
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HulaQuan_OldFashion/Models/HulaViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HulaQuan_OldFashion.Models
+{
+    public class HulaViewModels
+    {
+        public string UserName { get; set; }
+
+        public string UserPicture { get; set; }
+
+        public string Content { get; set; }
+
+        public List<ImageViewModels> Images { get; set; }
+
+
+    }
+
+    public class ImageViewModels
+    {
+        public string Original { get; set; }
+        public string Thumpnail { get; set; }
+    }
+}
